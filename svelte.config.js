f749ed1a1b2b8170304c01c6c4e69d861268894a
nelsojaco <42555701+nelsjac@users.noreply.github.com>
2022-04-28 15:19:39 -0500

    npm fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
+import { optimizeImports } from "carbon-preprocess-svelte";
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({}),
 	kit: {
 		adapter: adapter()
 	}
